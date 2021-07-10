65bef57d3949d8f6464b73098a93231f9bcc1517
sdinishiya <55315619+sdinishiya@users.noreply.github.com>
2021-07-10 10:09:41 +0530

    components folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import CustomizedTables from './components/Table'
+// import CustomizedTimeline from './components/forum/forum'
+// import RecipeReviewCard from './components/forum/card'
+// import Appointment from './components/forum/appointment'
 
 function App() {
   return (
+    <div style = {{ maxWidth : "90%", margin : "4rem auto" }}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
+    <Router>
+
+      <switch>
+        {/* <Route exact path = "/Table" component = {CustomizedTables} />
+        <Route exact path = "/forum/forum" component = {CustomizedTimeline} />
+        <Route exact path = "/forum/card" component = {RecipeReviewCard} />
+        <Route exact path = "/forum/appointment" component = {Appointment} /> */}
+      </switch>
+    </Router>
+   
+    </div>
+   
+
   );
 }
 
