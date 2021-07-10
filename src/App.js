f3a4e642ed2d2223fbc8b3e9f0ab1c5400e29071
MohomedSalman <mohamedsalmandeen@gmail.com>
2021-07-11 00:37:53 +0530

    signin&signup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-// import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import CustomizedTables from './components/Table'
-// import CustomizedTimeline from './components/forum/forum'
-// import RecipeReviewCard from './components/forum/card'
-// import Appointment from './components/forum/appointment'
+import "./App.css";
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div style = {{ maxWidth : "90%", margin : "4rem auto" }}>
-    <div className="App">
-     
-    </div>
-    <Router>
-
-      <switch>
-        {/* <Route exact path = "/Table" component = {CustomizedTables} />
-        <Route exact path = "/forum/forum" component = {CustomizedTimeline} />
-        <Route exact path = "/forum/card" component = {RecipeReviewCard} />
-        <Route exact path = "/forum/appointment" component = {Appointment} /> */}
-      </switch>
-    </Router>
-   
-    </div>
-   
-
+    <AppContainer>
+      <AccountBox />
+    </AppContainer>
   );
 }
 
