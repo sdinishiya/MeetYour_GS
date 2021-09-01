3d4b190ff46e63946d1f724a2855fa649771b669
hiradev <hirushachamod@gmail.com>
2021-09-01 16:47:22 +0530

    Revert "Revert "updated""
    This reverts commit 01ac6540ad03702986d1a7f5a239089a4d7a5cb7.

--- client/src/views/auth/Login.js
+++ client/src/views/auth/Login.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext, useState} from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { AccountContext } from "./accountContext";
 
 export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginStatus, setLoginStatus] = useState("");
+
+const login = ()=>{
+  axios.post("http://localhost:3000/auth/login",
+  { email: email,
+  password: password,
+
+}).then((response)=>{
+  if(response.data.message){
+    setLoginStatus(response.data.message);
+  }else{
+    setLoginStatus(response.data.message1);
+  }
+})
+}
+
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -56,6 +78,7 @@ export default function Login() {
                       type="email"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Email"
+                      onChange={(e)=>{ setEmail(e.target.value)}}
                     />
                   </div>
 
@@ -70,6 +93,7 @@ export default function Login() {
                       type="password"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Password"
+                      onChange={(e) => { setPassword(e.target.value)}}
                     />
                   </div>
                   <div>
@@ -89,6 +113,7 @@ export default function Login() {
                     <button
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="button"
+                      onClick={login}
                     >
                       Sign In
                     </button>
