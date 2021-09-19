5412a5864ead6dd696132d2caf03e47b4d615979
Maheshi Yatipansalawa <messi98ky@gmail.com>
2021-09-19 12:47:09 +0530

    Project Module Updated

--- client/src/components/Projects/Cards/PresentProjects_admin.js
+++ client/src/components/Projects/Cards/PresentProjects_admin.js
@@ -100,7 +100,7 @@ export default function ProjectReviewCard1(props) {
         delete_post(index.target.value);
      }
     }else if  (index.target.innerText=='EDIT'){
-      window.location.href="/EditProject"+index.target.value;
+      window.location.href="../EditProject/"+index.target.value;
       // alert("edit page");
     }
 
@@ -113,7 +113,7 @@ export default function ProjectReviewCard1(props) {
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" className={classes.avatar}>
-            PR
+            MYGS
           </Avatar>
         }
         action={
@@ -185,17 +185,7 @@ export default function ProjectReviewCard1(props) {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography paragraph>Read More</Typography>
-          <Typography paragraph>
-        a
-          </Typography>
-          <Typography paragraph>
-          a
-          </Typography>
-          <Typography paragraph>
-          a
-          </Typography>
-          <Typography>
-          a </Typography>
+          {props.readMore}
         </CardContent>
       </Collapse>
     </Card>
